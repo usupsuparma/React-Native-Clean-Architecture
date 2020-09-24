ac2c5bcff1b4187e059b3150906bcfbeb73b467a
usupsuparma <usup.suparma@bijb.co.id>
2020-09-24 17:00:30 +0700

    add components

--- src/core/presentation/BookComponent.js
+++ src/core/presentation/BookComponent.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import {Text, View, Button} from 'react-native';
 import getSampleBookData from './BookPresenter';
+import {Card} from '../../common/Card';
 
 const BookComponent = () => {
   const [bookNames, setBookNames] = useState([]);
+  const [count, setCounts] = useState(0);
 
   const getSampleBookDataHandler = async () => {
     const books = await getSampleBookData();
@@ -13,9 +15,18 @@ const BookComponent = () => {
   return (
     <View>
       <Button title="Get Sample Book" onPress={getSampleBookDataHandler} />
+      <Button
+        title="count"
+        onPress={() => {
+          setCounts(count + 1);
+        }}
+      />
       {bookNames.map((item) => (
-        <Text>{item.title}</Text>
+        <Card>
+          <Text>{item.title}</Text>
+        </Card>
       ))}
+      <Text>{count}</Text>
     </View>
   );
 };
